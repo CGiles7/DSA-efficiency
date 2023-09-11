e42dad9f3633c3e84a3ae30583587d1eae9382e8
Callie Giles <anchoredinthecrown@gmail.com>
2023-09-11 12:27:03 -0400

    add ftns to fulfill algorithms, make +efficient

--- src/cable.js
+++ src/cable.js
@@ -2,8 +2,43 @@
  The cable problem
  Implement algorithm 1 here
 */
-
-/**
- The cable problem
- Implement algorithm 2 here
-*/
+function cable1(currentLength, requiredLength, saleLength) {
+    if (currentLength >= requiredLength) {
+      return 0;
+    }
+    const distance = requiredLength - currentLength;
+    const count = Math.ceil(distance / saleLength);
+    return count;
+  }
+  
+  
+  // function cable1(currentLength, requiredLength, saleLength) {
+  //   let count = 0;
+  //   let i = currentLength;
+  //   while(i<requiredLength) {
+  //     i+=saleLength;
+  //     count++;
+  //   }
+  //   return count;
+  // }
+  /**
+   The cable problem
+   Implement algorithm 2 here
+  */
+  function cable2(currentLength, requiredLength, saleLength) {
+    if (currentLength >= requiredLength) {
+      return 0;
+    }
+    const distance = requiredLength - currentLength;
+    const total = Math.ceil(distance / saleLength);
+    return total;
+  }
+  
+  
+  // function cable2(currentLength, requiredLength, saleLength) {
+  //   let distance = requiredLength-currentLength;
+  //   let total = Math.ceil(distance/saleLength);
+  //   return total;
+  // }
+  
+  module.exports = {cable1, cable2};
--- src/missing.js
+++ src/missing.js
@@ -3,9 +3,58 @@
  The missing number problem
  Implement algorithm 1 here
 */
-
-
-/**
-The missing number problem
-Implement algorithm 2 here
-*/
+function missing1(A) {
+    const N = A.length + 1;
+    const seen = new Array(N + 1).fill(false);
+  
+    for (let i = 0; i < A.length; i++) {
+      seen[A[i]] = true;
+    }
+  
+    for (let i = 1; i <= N; i++) {
+      if (!seen[i]) {
+        return i;
+      }
+    }
+  }
+  
+  // function missing1(A) {
+  //   const N = A.length + 1;
+  //   for(let i=1; i<N; i++) {
+  //     let found = false;
+  //     let j = 0;
+  //     while(!found && j<A.length) {
+  //     if (i === A[j]) {
+  //       let found = true;
+  //       j++;
+  //     }
+  //   } if (!found) {
+  //     return i;
+  //   }
+  //   }
+  // }
+  
+  /**
+  The missing number problem
+  Implement algorithm 2 here
+  */
+  function missing2(A) {
+    let N = A.length + 1;
+    let fullSum = (N * (N + 1)) / 2;
+    let sum = 0;
+  
+    for (let i = 0; i < A.length; i++) {
+      sum += A[i];
+    }
+  
+    return fullSum - sum;
+  }
+  
+  // function missing2(A) {
+  //   let N = A.length + 1;
+  //   let fullSum = (N*(N+1))/2;
+  //   let sum = A.reduce((a, b) => a+b, 0);
+  //   return fullSum - sum;
+  // }
+  
+  module.exports = {missing1, missing2};
